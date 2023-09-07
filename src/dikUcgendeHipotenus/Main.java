d59701f0b28b653162b7753f032b8515c929bf67
Bora Kitirci <boraki2001@gmail.com>
2023-09-07 20:04:55 +0300

    feature added.

--- src/dikUcgendeHipotenus/Main.java
+++ src/dikUcgendeHipotenus/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         int a,b;
-        double c;
+        double c, cevre;
 
         Scanner input = new Scanner(System.in);
         System.out.print("1. Kenarı Giriniz: ");
@@ -13,5 +13,12 @@ public class Main {
 
         c = Math.sqrt((a*a + b*b));
         System.out.println("Hipotenüs: " + c);
+
+        cevre = (a+b+c);
+        double u = cevre / 2;
+        double alan = Math.sqrt(u*(u-a)*(u-b)*(u-c));
+
+        System.out.println("Üçgenin Çevresi : " + cevre);
+        System.out.println("Üçgenin Alanı : " + alan);
     }
 }
