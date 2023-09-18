54840e708eda3cb1fa9b04e9c2834be0f88eafb9
Bora Kitirci <boraki2001@gmail.com>
2023-09-18 14:51:48 +0300

    new project added.

---
+++ src/fibonacci/Main.java
@@ -0,0 +1,22 @@
+package fibonacci;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n, tempOne = 0, tempTwo = 1, total = 0;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Fibonacci serisinin eleman sayısı: ");
+        n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(tempOne);
+            total = tempOne + tempTwo;
+            tempOne = tempTwo;
+            tempTwo = total;
+        }
+
+
+    }
+}
